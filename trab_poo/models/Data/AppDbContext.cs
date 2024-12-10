58ba28a3f02ed0b09a5cae688d7df15c40bf6760
rodrigoferreiramoura <rodrigo.fmoura04@gmail.com>
2024-12-10 18:59:24 +0000

    add the entity frame

--- trab_poo/models/Data/AppDbContext.cs
+++ trab_poo/models/Data/AppDbContext.cs
@@ -1,14 +1,18 @@
 using Microsoft.EntityFrameworkCore;  // For DbContext and DbSet
-using ModelsPOO.Data;
+using ModelsPOO.models.Data;
 
 namespace ModelsPOO.models.Data;
 
 
 
 
-    public class AppDbContext : DbContext
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) 
+        : base(options)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        public DbSet<YourEntity> YourEntities { get; set; }
     }
+
+    // Define your DbSets (tables) here
+    //public DbSet<YourEntity> YourEntities { get; set; }
 }
